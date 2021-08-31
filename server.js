1314dafa288f9e38754d8de267152bd9c80885d3
suthyscott <sutherlandscott21@gmail.com>
2021-08-31 12:35:11 -0600

    added rollbar errors

--- server.js
+++ server.js
@@ -11,21 +11,31 @@ const rollbar = new Rollbar({
 const students = []
 const app = express()
 app.use(express.json())
+app.use('/style', express.static('./public/styles.css'))
 
 app.get('/', (req,res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'))
     rollbar.info('html file served successfully.')
 })
 
-app.post('/api/student', (req, res)=> {
+app.post('/api/student', (req, res)=>{
     let {name} = req.body
     name = name.trim()
 
-    students.push(name)
+    const index = students.findIndex(studentName=> studentName === name)
 
-    rollbar.log('Student added successfully', {author: 'Scott', type: 'manual entry'})
+    if(index === -1 && name !== ''){
+        students.push(name)
+        rollbar.log('Student added successfully', {author: 'Scott'})
+        res.status(200).send(students)
+    } else if (name === ''){
+        rollbar.error('No name given')
+        res.status(400).send('must provide a name.')
+    } else {
+        rollbar.error('student already exists')
+        res.status(400).send('that student already exists')
+    }
 
-    res.status(200).send(students)
 })
 const port = process.env.PORT || 4545
 
