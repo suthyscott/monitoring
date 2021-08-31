c2f60a313d0c667aeefb0984ef7367243c7b35c0
suthyscott <sutherlandscott21@gmail.com>
2021-08-31 12:19:00 -0600

    added json parser

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const rollbar = new Rollbar({
 
 const students = []
 const app = express()
+app.use(express.json())
 
 app.get('/', (req,res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'))
